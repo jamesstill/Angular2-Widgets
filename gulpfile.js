fef0f542fa76ab6b1dc9f2011e06034b1a15d7aa
STILL James C <stillj@co.multnomah.or.us>
2016-10-26 16:13:32 -0700

    gulpfile changes

--- gulpfile.js
+++ gulpfile.js
@@ -55,25 +55,6 @@ gulp.task("libs", function () {
         .pipe(gulp.dest("build/lib"));
 });
 
-/**
- * Watch for changes in TypeScript, HTML and CSS files.
- */
-//gulp.task('watch', function () {
-//    gulp.watch(["src/**/*.ts"], ['compile']).on('change', function (e) {
-//        console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
-//    });
-//    gulp.watch(["src/**/*.html", "src/**/*.css"], ['resources']).on('change', function (e) {
-//        console.log('Resource file ' + e.path + ' has been changed. Updating.');
-//    });
-//});
-
-/**
- * Build the project.
- */
-//gulp.task("build", ['compile', 'resources', 'libs'], function () {
-//    console.log("Building the project ...");
-//});
-
 gulp.task("build", ['resources', 'libs'], function () {
     console.log("Building the project ...");
 });
